378e232fed3857437f4d307e613be960bd7fb37f
B Seamus Haney <43055154+Bhaney44@users.noreply.github.com>
2019-04-16 11:38:02 -0400

    Add files via upload

--- Quantum_MNIST.py
+++ Quantum_MNIST.py
@@ -1,163 +1,73 @@
-# Define the functions required in this example
-def metric(y, y_pred):
-    """
-    :param y: true label
-    :param y_pred: predicted label
-    :return: metric score
-    """
+#NOTICE
+#Code is buggy and does not work yet
 
-    return metrics.accuracy_score(y, y_pred)
+import numpy as np
+from sklearn.datasets import load_digits
+
+# Load data set
+dig = load_digits()
+
+# Shuffle data for random selection for training and test data
+idx = np.arange(len(dig.target))
+np.random.shuffle(idx)
+
+# Use 2/3 of data set for training, 1/3 for testing
+idx_train = idx[:2*len(idx)//3]
+idx_test = idx[2*len(idx)//3:]
+
+# Set up data points for training and testing
+X_train = dig.data[idx_train]
+X_test = dig.data[idx_test]
+
+# Set up labels for training and testing.
+y_train = 2 * dig.target[idx_train] - 1  
+y_test = 2 * dig.target[idx_test] - 1
 
 
-def train_model(X_train, y_train, X_test, y_test, lmd):
-    """
-    :param X_train: training data
-    :param y_train: training label
-    :param X_test: testing data
-    :param y_test: testing label
-    :param lmd: lambda used in regularization
-    :return:
-    """
+#Dataset information
 
-    # define parameters used in this function
+print("Training data size: \t%d samples with %d features" %(X_train.shape[0], X_train.shape[1]))
+print("Testing data size: \t%d samples" %(X_test.shape[0]))
+print('---------------------------------------')
+
+
+
+#QBoost and QBoost Plus are quantum algorithms
+#QBoost and QBoost Plus require DWave resources to successfully run
+#QBoost Function
+
+def QBoost(X_train, y_train, X_test, y_test):
     NUM_READS = 1000
-    NUM_WEAK_CLASSIFIERS = 30
-    TREE_DEPTH = 2
     DW_PARAMS = {'num_reads': NUM_READS,
                  'auto_scale': True,
                  'num_spin_reversal_transforms': 10,
                  'postprocess': 'optimization',
                  }
 
-    # define sampler
-    dwave_sampler = DWaveSampler(solver={'qpu': True})
+    from dwave.system.samplers import DWaveSampler
+    from dwave.system.composites import EmbeddingComposite
+
+    dwave_sampler = DWaveSampler(solver={'qpu': True}) 
     emb_sampler = EmbeddingComposite(dwave_sampler)
 
-    N_train = len(X_train)
-    N_test = len(X_test)
-    print("\n======================================")
-    print("Train size: %d, Test size: %d" %(N_train, N_test))
-    print('Num weak classifiers:', NUM_WEAK_CLASSIFIERS)
+    from qboost import WeakClassifiers, QBoostClassifier
 
-    # Preprocessing data
-    imputer = preprocessing.Imputer()
-    scaler = preprocessing.StandardScaler()
-    normalizer = preprocessing.Normalizer()
-
-    X_train = scaler.fit_transform(X_train)
-    X_train = normalizer.fit_transform(X_train)
-
-    X_test = scaler.fit_transform(X_test)
-    X_test = normalizer.fit_transform(X_test)
-
-    ## Adaboost
-    print('\nAdaboost')
-    clf1 = AdaBoostClassifier(n_estimators=NUM_WEAK_CLASSIFIERS)
-    clf1.fit(X_train, y_train)
-    y_train1 = clf1.predict(X_train)
-    y_test1 = clf1.predict(X_test)
-#     print(clf1.estimator_weights_)
-    print('accu (train): %5.2f'%(metric(y_train, y_train1)))
-    print('accu (test): %5.2f'%(metric(y_test, y_test1)))
-
-    # Ensembles of Decision Tree
-    print('\nDecision tree')
-    clf2 = WeakClassifiers(n_estimators=NUM_WEAK_CLASSIFIERS, max_depth=TREE_DEPTH)
-    clf2.fit(X_train, y_train)
-    y_train2 = clf2.predict(X_train)
-    y_test2 = clf2.predict(X_test)
-#     print(clf2.estimator_weights)
-    print('accu (train): %5.2f' % (metric(y_train, y_train2)))
-    print('accu (test): %5.2f' % (metric(y_test, y_test2)))
-    
-    # Random forest
-    print('\nRandom Forest')
-    clf3 = RandomForestClassifier(max_depth=TREE_DEPTH, n_estimators=NUM_WEAK_CLASSIFIERS)
-    clf3.fit(X_train, y_train)
-    y_train3 = clf3.predict(X_train)
-    y_test3 = clf3.predict(X_test)
-    print('accu (train): %5.2f' % (metric(y_train, y_train3)))
-    print('accu (test): %5.2f' % (metric(y_test, y_test3)))
-
-    # Qboost
-    print('\nQBoost')
-    clf4 = QBoostClassifier(n_estimators=NUM_WEAK_CLASSIFIERS, max_depth=TREE_DEPTH)
-    clf4.fit(X_train, y_train, emb_sampler, lmd=lmd, **DW_PARAMS)
+    clf4 = QBoostClassifier(n_estimators=30, max_depth=2)
+    clf4.fit(X_train, y_train, emb_sampler, lmd=1.0, **DW_PARAMS)
     y_train4 = clf4.predict(X_train)
     y_test4 = clf4.predict(X_test)
-    print(clf4.estimator_weights)
-    print('accu (train): %5.2f' % (metric(y_train, y_train4)))
-    print('accu (test): %5.2f' % (metric(y_test, y_test4)))
 
-    # QboostPlus
-    print('\nQBoostPlus')
-    clf5 = QboostPlus([clf1, clf2, clf3, clf4])
-    clf5.fit(X_train, y_train, emb_sampler, lmd=lmd, **DW_PARAMS)
-    y_train5 = clf5.predict(X_train)
-    y_test5 = clf5.predict(X_test)
-    print(clf5.estimator_weights)
-    print('accu (train): %5.2f' % (metric(y_train, y_train5)))
-    print('accu (test): %5.2f' % (metric(y_test, y_test5)))
+    from sklearn.metrics import accuracy_score
 
-    print("===========================================================================")
-    print("Method \t Adaboost \t DecisionTree \t RandomForest \t Qboost \t Qboost+")
-    print("Train\t %5.2f \t\t %5.2f \t\t %5.2f \t\t %5.2f \t\t %5.2f"% (metric(y_train, y_train1),
-                                                                         metric(y_train, y_train2),
-                                                                         metric(y_train, y_train3),
-                                                                         metric(y_train, y_train4),
-                                                                         metric(y_train, y_train5),
-                                                                        ))
-    print("Test\t %5.2f \t\t %5.2f \t\t %5.2f \t\t %5.2f \t\t %5.2f"% (metric(y_test, y_test1),
-                                                                       metric(y_test, y_test2),
-                                                                       metric(y_test, y_test3),
-                                                                       metric(y_test, y_test4),
-                                                                       metric(y_test, y_test5)))
-    print("===========================================================================")
+    print('Accuracy for training data: \t', (accuracy_score(y_train, y_train4)))
+    print('Accuracy for test data: \t', (accuracy_score(y_test, y_test4)))
     
-    return [clf1, clf2, clf3, clf4, clf5]
+    return clf4
+    
+clf4 = QBoost(X_train, y_train, X_test, y_test)
 
+#Print Results
+print('QBoost: ')
+clf4 = QBoost(X_train, y_train, X_test, y_test) 
+print('---------------------------------------')
 
-
-# import necessary packages
-from sklearn import preprocessing, metrics
-from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
-from sklearn.datasets import load_breast_cancer, fetch_openml
-from dwave.system.samplers import DWaveSampler
-from dwave.system.composites import EmbeddingComposite
-
-from qboost import WeakClassifiers, QBoostClassifier, QboostPlus
-
-import numpy as np
-#import os
-import matplotlib.pyplot as plt
-
-mnist = fetch_openml('mnist_784', version=1)
-
-idx = np.arange(len(mnist['data']))
-np.random.shuffle(idx)
-
-n = 15000
-idx = idx[:n]
-idx_train = idx[:2*n//3]
-idx_test = idx[2*n//3:]
-
-X_train = mnist['data'][idx_train]
-X_test = mnist['data'][idx_test]
-
-y_train = 2*(mnist['target'][idx_train] >= '4') - 1
-y_test = 2*(mnist['target'][idx_test] >= '4') - 1
-
-print("Training data size: (%d, %d)" %(X_train.shape))
-print("Testing data size: (%d, %d)" %(X_test.shape))
-
-for i in range(16):
-    if y_train[i] == 1:
-        COLORMAP = 'gray'
-    else:
-        COLORMAP = 'gray_r'
-    plt.subplot(4,4, i+1)
-    plt.imshow(X_train[i].reshape(28, 28), cmap=COLORMAP)
-    plt.axis('off')
-
-# start training the model
-clfs = train_model(X_train, y_train, X_test, y_test, 1.0)
